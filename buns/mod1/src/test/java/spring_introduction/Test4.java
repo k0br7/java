dbf039b80ed841632b46e5145a71858195d209c2
k0br7 <103520505+k0br7@users.noreply.github.com>
2023-11-07 18:24:48 +0500

    Test4.java

--- buns/mod1/src/test/java/spring_introduction/Test4.java
+++ buns/mod1/src/test/java/spring_introduction/Test4.java
@@ -7,19 +7,6 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2");
 
-//          Dog myDog = context.getBean("myPet", Dog.class);
-//          myDog.setName("Belka");
-//          Dog yourDog = context.getBean("myPet", Dog.class);
-//          yourDog.setName("Strelka");
-//
-//          System.out.println(myDog.getName());
-//          System.out.println(yourDog.getName());
-
-
-//          System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog==yourDog));
-//          System.out.println(myDog);
-//          System.out.println(yourDog);
-
         context.close();
 
     }
