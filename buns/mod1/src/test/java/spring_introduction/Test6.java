d73cd564e24e026de16a7050b8041fdc4b7aa53b
k0br7 <103520505+k0br7@users.noreply.github.com>
2023-11-07 18:25:05 +0500

    Test6.java

--- buns/mod1/src/test/java/spring_introduction/Test6.java
+++ buns/mod1/src/test/java/spring_introduction/Test6.java
@@ -7,17 +7,11 @@ public class Test6 {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
-//        Pet cat1 = context.getBean("catBean", Pet.class);
-//        Pet cat2 = context.getBean("catBean", Pet.class);
-//        cat.say();
-
 
         Person person = context.getBean("personBean", Person.class);
         System.out.println(person.getSurname());
         System.out.println(person.getAge());
 
-//        Person person2 = context.getBean("personBean", Person.class);
-//        person.callYourPet();
 
         context.close();
     }
